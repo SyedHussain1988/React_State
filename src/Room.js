868a89d40a664bc43bbfe4edd0e919fd96d5aba4
SyedHussainAhmed121 <shas_virgo121@yahoo.com>
2020-06-12 01:47:11 +0500

    updated app.js

--- src/Room.js
+++ src/Room.js
@@ -5,26 +5,32 @@ function Room() {
 
     //const state = useState(true);
     //console.log("State = ",state);
-    let [isLit, setLit] = useState(false);
-    let [age , setAge] = useState(24);
+    //let [isLit, setLit] = useState(false);
+
+    let [isLitON, setLitON] = useState(true);
+    let [tempF , setTempFarh] = useState(72);
+
+    //let [age , setAge] = useState(24);
     
     //let hello = 'Hello '+ isLit +'World';
     //let hello = `Hello ${isLit} World`;
 
     return (
-    <div className={`room ${isLit? "lit": "dark"}`}> 
-        This Room is {isLit? "lit": "dark"}
+    //<div className={`room ${isLit? "lit": "dark"}`}> 
+    <div className={`room ${isLitON? "ON": "OFF"}`}> 
+        Room Light is {isLitON? "ON": "OFF"}
         <br/>
-        Age: {age}
+        <button onClick={()=> setLitON(true) } >ON</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button onClick={()=> setLitON(false) } >OFF</button>
         <br/>
-        <button onClick={()=> setLit(!isLit) } >Toggle Light</button>
+        Room Current Temperature is: {tempF}
         <br/>
-        <button onClick={ ()=> {
+        <button title="Increase Temperature" onClick={()=> setTempFarh(++tempF)}>+</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button title="Decrease Temperature" onClick={()=> setTempFarh(--tempF)}>-</button>
+        {/* <button onClick={ ()=> {
                         console.log("Increase age arrow function");
                         setAge(++age);
                         }}>
             Increase Age
-        </button>
+        </button> */}
     </div>
     );
 }
